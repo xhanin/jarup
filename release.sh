13b420422e60ceba3fa90d2b4803450ede319a17
xhanin <xavier.hanin@4sh.fr>
2014-01-11 09:01:07 +0100

    add release script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: release.sh <version>"
+  exit 1
+fi
+
+echo "releasing $1"
+
+rm -rf release
+mkdir release
+cd release
+git clone git@github.com:xhanin/jarup.git .
+
+sed -i '' s/0.01-SNAPSHOT/$1/g pom.xml
+
+mvn clean package
+
+cp target/jarup-$1.jar dist/jarup.jar
+
+git checkout -- pom.xml
+git add .
+git commit -m "release $1"
+git tag -a $1 -m "release $1"
+git push origin
+git push --tags origin
