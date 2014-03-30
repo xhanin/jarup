020da7bef044fb30aa46d6ac79c21c13c2c447d5
xhanin <xavier.hanin@gmail.com>
2014-03-30 11:31:45 +0200

    FIX curl on rawgithub
    it uses a redirect now

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ ! -f  ~/.jarup/jarup.jar ]; then
     echo "installing jarup"
     mkdir -p ~/.jarup && cd ~/.jarup
     echo "downloading jarup.jar"
-    curl -O https://rawgithub.com/xhanin/jarup/master/dist/jarup.jar
+    curl -OL https://rawgithub.com/xhanin/jarup/master/dist/jarup.jar
     java -jar jarup.jar gen-script
 
     echo "creating a link in /usr/local/bin for convenient use"
@@ -15,7 +15,7 @@ else
     cd ~/.jarup
     rm jarup.jar && rm jarup
     echo "downloading jarup.jar"
-    curl -O https://rawgithub.com/xhanin/jarup/master/dist/jarup.jar
+    curl -OL https://rawgithub.com/xhanin/jarup/master/dist/jarup.jar
     java -jar jarup.jar gen-script
     echo "jarup updated"
 fi
